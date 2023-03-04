9b0cf85b0b36370976d7eb290e4842b47e206376
H.Halil Olcay <holcay@marun.edu.tr>
2023-03-04 13:31:57 +0300

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+def impute_missing_vaccinations(filename):
+    df = pd.read_csv(filename)
+
+    min_daily_vaccinations = df.groupby('country')['daily_vaccinations'].min()
+
+    df['daily_vaccinations'] = df['daily_vaccinations'].fillna(0)
+
+    for country in df['country'].unique():
+        relevant_countries = df[df['country'] != country]['country']
+        relevant_min = min_daily_vaccinations[relevant_countries].min()
+        idx = (df['country'] == country) & (df['daily_vaccinations'].isna())
+        df.loc[idx, 'daily_vaccinations'] = relevant_min
+
+    top_countries = df.groupby('country')['daily_vaccinations'].median().nlargest(3).index.tolist()
+    print("Top-3 countries with highest median daily vaccination numbers: ", top_countries)
+
+    return df
